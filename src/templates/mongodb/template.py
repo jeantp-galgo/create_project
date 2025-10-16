db1f2b43997b12ed4eea61cdb69aedafb5f61dca
Jean <jtrujillo@galgo.com>
2025-10-16 12:33:05 -0500

    feat(get_service_content): se agrega contenido del servicio de mongodb

--- src/templates/mongodb/template.py
+++ src/templates/mongodb/template.py
@@ -5,6 +5,53 @@ Template para crear un servicio de MongoDB.
 class MongodbTemplate:
 
     @staticmethod
+    def get_service_content():
+        return """
+import mongodb.mongodb_connection as connection
+from mongodb.pipelines.extract_variation_data import extract_variation_data
+from mongodb.pipelines.extract_publications_data import extract_publications_data
+from mongodb.pipelines.extract_info_titles import extract_info_titles_data
+
+class MongoDBFunctions:
+    def __init__(self):
+        self.client = connection.connect_mongodb()
+
+    def list_collections(self):
+        db = self.client.db_marketplace
+        return db.list_collection_names()
+
+    def select_collection(self, collection_name):
+        db = self.client.db_marketplace
+        collection = db[collection_name]
+        return collection
+
+    def variation_data_aggregation(self, collection_name, countryCode, vehicle_type):
+        if countryCode not in ["MX", "CO", "CL", "PE"] or vehicle_type not in ["MOTORCYCLES", "CARS"]:
+            print(f"Country code {countryCode} or vehicle type {vehicle_type} not supported")
+            return None
+        else:
+            pipeline = extract_variation_data(countryCode, vehicle_type)
+            collection = self.select_collection(collection_name)
+            results = list(collection.aggregate(pipeline))
+
+        return results
+
+    def publications_data_aggregation(self, collection_name):
+        pipeline = extract_publications_data()
+        collection = self.select_collection(collection_name)
+        results = list(collection.aggregate(pipeline))
+
+        return results
+
+    def publications_title_data_aggregation(self, collection_name, countryCode, vehicle_type):
+        pipeline = extract_info_titles_data(countryCode, vehicle_type)
+        collection = self.select_collection(collection_name)
+        results = list(collection.aggregate(pipeline))
+
+        return results
+        """
+
+    @staticmethod
     def get_config_content():
         return """
 from pymongo import MongoClient
